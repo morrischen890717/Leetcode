5fa23a7f2040177d2cbbeb2e2f8865e11e121e57
陳漱宇 <morris@chenshuyudeMacBook-Pro.local>
2023-04-07 17:13:42 +0800

    add the solution(using DFS) for Problem 1020(Number of Enclaves)

---
+++ C++/1020.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void recursion(vector<vector<int>>& grid, int m, int n, int r, int c){
+        if(r < 0 || r >= m || c < 0 || c >= n || grid[r][c] == 0 || grid[r][c] == -1)
+            return;
+        else{
+            grid[r][c] = -1;
+            recursion(grid, m, n, r - 1, c);
+            recursion(grid, m, n, r + 1, c);
+            recursion(grid, m, n, r, c - 1);
+            recursion(grid, m, n, r, c + 1);
+        }
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        int ans = 0;
+        for(int i = 0; i < m; i++){
+            recursion(grid, m, n, i, 0);
+            recursion(grid, m, n, i, n - 1);
+        }
+        for(int i = 0; i < n; i++){
+            recursion(grid, m, n, 0, i);
+            recursion(grid, m, n, m - 1, i);
+        }
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j] == 1)
+                    ans++;
+            }
+        }
+        return ans;
+    }
+};
