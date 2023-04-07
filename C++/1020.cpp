ca7fb076ccc69b54bea3e7c8374e025de2d89183
陳漱宇 <morris@chenshuyudeMacBook-Pro.local>
2023-04-07 17:21:56 +0800

    add the solution(using DFS) for Problem 1020(Number of Enclaves)

--- C++/1020.cpp
+++ C++/1020.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-    void recursion(vector<vector<int>>& grid, int m, int n, int r, int c){
+    void dfs(vector<vector<int>>& grid, int m, int n, int r, int c){
         if(r < 0 || r >= m || c < 0 || c >= n || grid[r][c] == 0 || grid[r][c] == -1)
             return;
         else{
             grid[r][c] = -1;
-            recursion(grid, m, n, r - 1, c);
-            recursion(grid, m, n, r + 1, c);
-            recursion(grid, m, n, r, c - 1);
-            recursion(grid, m, n, r, c + 1);
+            dfs(grid, m, n, r - 1, c);
+            dfs(grid, m, n, r + 1, c);
+            dfs(grid, m, n, r, c - 1);
+            dfs(grid, m, n, r, c + 1);
         }
         return;
     }
     int numEnclaves(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        int ans = 0;
+        int moves = 0;
         for(int i = 0; i < m; i++){
-            recursion(grid, m, n, i, 0);
-            recursion(grid, m, n, i, n - 1);
+            dfs(grid, m, n, i, 0);
+            dfs(grid, m, n, i, n - 1);
         }
         for(int i = 0; i < n; i++){
-            recursion(grid, m, n, 0, i);
-            recursion(grid, m, n, m - 1, i);
+            dfs(grid, m, n, 0, i);
+            dfs(grid, m, n, m - 1, i);
         }
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
                 if(grid[i][j] == 1)
-                    ans++;
+                    moves++;
             }
         }
-        return ans;
+        return moves;
     }
 };
