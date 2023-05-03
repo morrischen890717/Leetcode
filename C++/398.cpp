f0b51fdaa2e331d068746f097679cd45698d6d8d
陳漱宇 <morris@chenshuyudeMacBook-Pro.local>
2023-05-03 16:07:52 +0800

    add the solution(using unordered_map and rand()) for Problem 398(Random Pick Index)

---
+++ C++/398.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    unordered_map<int, vector<int>> m;
+    
+    Solution(vector<int>& nums) {
+        srand(time(NULL));
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            m[nums[i]].push_back(i);
+        }
+    }
+    
+    int pick(int target) {
+        int n = m[target].size();
+        return m[target][rand() % n];
+    }
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * int param_1 = obj->pick(target);
+ */
