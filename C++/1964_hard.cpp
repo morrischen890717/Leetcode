680588c2f925e5a212021a75a9ef2c0ac2bdd757
陳漱宇 <morris@chenshuyudeMBP.Home>
2023-05-07 17:20:08 +0800

    add the solution(using binary search) for Hard Problem 1964(Find the Longest Valid Obstacle Course at Each Position)

---
+++ C++/1964_hard.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> longestObstacleCourseAtEachPosition(vector<int>& obstacles) {
+        int n = obstacles.size();
+        vector<int> ans;
+        vector<int> increase_subseq(n);
+        int len = 0;
+        for(int i = 0; i < n; i++){
+            int l = 0, r = len - 1; 
+            while(l <= r){  // using binary search to find the position which obstacles[i] should insert/replace
+                int mid = (l + r) / 2;
+                if(obstacles[i] >= increase_subseq[mid])
+                    l = mid + 1;
+                else
+                    r = mid - 1;
+            }
+            increase_subseq[l] = obstacles[i]; // if l == len, then insert to back. Otherwise, replace
+            ans.push_back(l + 1);
+            if(l == len)
+                len++;
+        }
+        return ans;
+    }
+};
