9ab96e63229929232020056b29a30eba7274c7c4
陳漱宇 <morris@chenshuyudeMBP.Home>
2023-05-27 21:05:25 +0800

    add three solutions(3 solutions are similar, sol1: top-down DP, sol2: bottom-up DP, sol3: bottom-up DP with O(N) space) for Problem 486(Predict the Winner)

--- C++/486_sol3.cpp
+++ C++/486_sol3.cpp
@@ -3,7 +3,7 @@ public:
     bool PredictTheWinner(vector<int>& nums) {
         // space: O(N)
         int n = nums.size();
-        vector<int> dp(n + 1, 0);
+        vector<int> dp(n + 1, 0); // dp[j] means the value that the player who does first take can win over another player from nums[j] ~ nums[i]
         for(int i = 0; i < n; i++){
             for(int j = i; j >= 0; j--){
                 dp[j] = max(nums[j] - dp[j + 1], nums[i] - dp[j]);
