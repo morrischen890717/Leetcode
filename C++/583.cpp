40c74cc31aae167088e88b805667a1d9e0f68ee9
陳漱宇 <morris@chenshuyudeMacBook-Pro.local>
2023-06-08 20:06:35 +0800

    add the solution(using DP) for Problem 583(Delete Operation for Two Strings)

---
+++ C++/583.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int len1 = word1.length(), len2 = word2.length();
+        vector<vector<int>> dp(2, vector<int> (len2 + 1, 0));
+        for(int j = 1; j <= len2; j++){
+            dp[0][j] = j;
+        }
+        for(int i = 1; i <= len1; i++){
+            dp[i % 2][0] = i;
+            for(int j = 1; j <= len2; j++){
+                if(word1[i - 1] == word2[j - 1])
+                    dp[i % 2][j] = min({dp[(i - 1) % 2][j - 1], dp[(i - 1) % 2][j] + 1, dp[i % 2][j - 1] + 1});
+                else
+                    dp[i % 2][j] = min(dp[(i - 1) % 2][j] + 1, dp[i % 2][j - 1] + 1);
+            }
+        }
+        return dp[len1 % 2][len2];
+    }
+};
