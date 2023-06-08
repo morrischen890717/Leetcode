867adccc46b4cc1adb5463e16097d5c8394ddaed
陳漱宇 <morris@chenshuyudeMacBook-Pro.local>
2023-06-08 20:14:17 +0800

    add the solution(using DP) for Problem 583(Delete Operation for Two Strings)

--- C++/583.cpp
+++ C++/583.cpp
@@ -9,10 +9,7 @@ public:
         for(int i = 1; i <= len1; i++){
             dp[i % 2][0] = i;
             for(int j = 1; j <= len2; j++){
-                if(word1[i - 1] == word2[j - 1])
-                    dp[i % 2][j] = min({dp[(i - 1) % 2][j - 1], dp[(i - 1) % 2][j] + 1, dp[i % 2][j - 1] + 1});
-                else
-                    dp[i % 2][j] = min(dp[(i - 1) % 2][j] + 1, dp[i % 2][j - 1] + 1);
+                dp[i % 2][j] = (word1[i - 1] == word2[j - 1] ? dp[(i - 1) % 2][j - 1] : min(dp[(i - 1) % 2][j] + 1, dp[i % 2][j - 1] + 1));
             }
         }
         return dp[len1 % 2][len2];
