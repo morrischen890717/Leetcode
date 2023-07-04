a87bd0f20a118a8d7189706e41d03a6f8818053e
陳漱宇 <morris@chenshuyudeMBP.Home>
2023-07-04 16:06:48 +0800

    add the solution(using Greedy) for Problem 670(Maximum Swap)

---
+++ C++/670.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maximumSwap(int num) {
+        string s = to_string(num);
+        int len = s.length();
+        int max_digit = -1, max_idx = -1;
+        int left_idx = -1, right_idx = -1;
+        for(int i = len - 1; i >= 0; i--){
+            if(s[i] > max_digit){
+                max_digit = s[i];
+                max_idx = i;
+
+            }
+            else if(s[i] < max_digit){
+                left_idx = i;
+                right_idx = max_idx;
+            }
+        }
+        if(left_idx != -1)
+            swap(s[left_idx], s[right_idx]);
+        return stoi(s);
+    }
+};
