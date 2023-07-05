2f38eee84c79dabfcf92ec613406ad6171c9cfba
陳漱宇 <morris@chenshuyudeMacBook-Pro.local>
2023-07-05 19:27:11 +0800

    add the solution for Problem 672(Bulb Switcher II)

--- C++/672.cpp
+++ C++/672.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int flipLights(int n, int presses) {
+        // reference: https://leetcode.com/problems/bulb-switcher-ii/solutions/107269/java-o-1/
         if(presses == 0)
             return 1;
         else if(n == 1)
