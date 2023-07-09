a5ec2feb3b01e2ab12fc2c3c11292c96c72eab32
陳漱宇 <morris@chenshuyudeMBP.Home>
2023-07-09 16:25:20 +0800

    add the solution(using unordered_map and priority_queue) for Problem 692(Top K Frequent Words)

---
+++ C++/692.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string, int> cnt;
+        for(string& word: words){
+            cnt[word]++;
+        }
+        priority_queue<pair<int, string>> pq; // {-count, word}        
+        for(unordered_map<string, int>::iterator it = cnt.begin(); it != cnt.end(); it++){
+            pq.push({-it->second, it->first});
+            while(pq.size() > k)
+                pq.pop();
+        }
+        vector<string> ans(k);
+        for(int i = k - 1; i >= 0; i--){ // from back to front
+            ans[i] = pq.top().second;
+            pq.pop();
+        }
+        return ans;
+    }
+};
