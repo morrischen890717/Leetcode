8d3e7de7d4098208d3bf03aee6dec0e3b48a8133
陳漱宇 <morris@chenshuyudeMBP.Home>
2023-09-30 14:51:43 +0800

    add the solution(using BFS) for Problem 1162(As Far from Land as Possible)

---
+++ C++/1162.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& grid) {
+        // using BFS
+        int m = grid.size(), n = grid[0].size();
+        int cnt = 0;
+        vector<int> dirs = {-1, 0, 1, 0, -1};
+        queue<pair<int, int>> q;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(grid[i][j]){
+                    cnt++;
+                    q.push({i, j});
+                }
+            }
+        }
+        if(cnt == 0 || cnt == m * n)
+            return -1;
+        int dist = 0;
+        while(!q.empty()){
+            int size = q.size();
+            for(int i = 0; i < size; i++){
+                pair<int, int> p = q.front();
+                int x = p.first, y = p.second;
+                q.pop();
+                for(int j = 0; j < 4; j++){
+                    int new_x = x + dirs[j], new_y = y + dirs[j + 1];
+                    if(new_x >= 0 && new_x < m && new_y >= 0 && new_y < n && !grid[new_x][new_y]){
+                        grid[new_x][new_y] = 1; // visited
+                        q.push({new_x, new_y});
+                    }
+                }
+            }
+            dist++;
+        }
+        return dist - 1;
+    }
+};
