b48ddfe2440b4a40934f5dbc896216d2874d0cfd
陳漱宇 <morris@chenshuyudeMBP.Home>
2023-10-01 17:11:02 +0800

    add the solution for Problem 1171(Remove Zero Sum Consecutive Nodes from Linked List)

---
+++ C++/1171.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeZeroSumSublists(ListNode* head) {
+        // time complexity: O(N)
+        // reference: https://leetcode.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list/solutions/366319/java-c-python-greedily-skip-with-hashmap/
+        ListNode *root = new ListNode(0, head); // add root to prevent that head is removed
+        ListNode *cur = root;
+        unordered_map<int, ListNode*> seen;
+        int prefix = 0;
+        while(cur){
+            prefix += cur->val;
+            seen[prefix] = cur;
+            cur = cur->next;
+        }
+        cur = root;
+        prefix = 0;
+        for(cur = root; cur; cur = cur->next){
+            prefix += cur->val;
+            if(seen.count(prefix) > 0 && cur != seen[prefix])
+                cur->next = seen[prefix]->next;
+        }
+        return root->next;
+    }
+};
