307cb4480754c147070e2fba8e023346cd66b6bf
陳漱宇 <morris@chenshuyudeMBP.Home>
2023-10-11 21:10:16 +0800

    add the solution(using unordered_set) for Problem 217(Contains Duplicate)

---
+++ C++/217.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> s;
+        for(int num: nums){
+            if(s.count(num) > 0)
+                return true;
+            s.insert(num);
+        }
+        return false;
+    }
+};
