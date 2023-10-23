ee26434e5ae65ac3b63a1aeeeebc14e0c7ff22ec
陳漱宇 <morris@chenshuyudeMBP.Home>
2023-10-23 17:33:10 +0800

    add the solution(using math) for Problem 342(Power of Four)

--- C++/342.cpp
+++ C++/342.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
+        // using math
+        // reference: https://leetcode.com/problems/power-of-four/solutions/80460/1-line-c-solution-without-confusing-bit-manipulations/?envType=daily-question&envId=2023-10-23
         return n > 0 && (n & (n - 1)) == 0 && (n - 1) % 3 == 0;
     }
 };
