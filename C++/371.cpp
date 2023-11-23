d8b6ed51e657ac167a84a0925339ab8ee99358e4
陳漱宇 <morris@chenshuyudeMBP.Home>
2023-11-24 00:45:34 +0800

    add the solution(using math and bit manipulation) for Problem 371(Sum of Two Integers)

--- C++/371.cpp
+++ C++/371.cpp
@@ -1,16 +1,14 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        int ans = 0;
-        int i = 0;
+        // using math and bit manipulation
+        // reference: https://leetcode.com/problems/sum-of-two-integers/solutions/132479/simple-explanation-on-how-to-arrive-at-the-solution/?envType=featured-list&envId=top-interview-questions%3FenvType%3Dfeatured-list&envId=top-interview-questions
         int carry = 0;
-        while((a || b || carry) && i < 32){ // i < 32 is for the situation of k+(-k)
-            ans |= ((((a & 1) ^ (b & 1)) ^ carry) << i);
-            carry = (((a & 1) & (b & 1)) | ((a & 1) & carry) | (carry & (b & 1)));
-            a >>= 1;
-            b >>= 1;
-            i++;
+        while(b != 0){
+            carry = (a & b);
+            a ^= b;
+            b = (carry << 1);
         }
-        return ans;
+        return a;
     }
 };
