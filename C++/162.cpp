a81da57ebee534d75047eccd677f12317dcd793c
陳漱宇 <morris@chenshuyudeMBP.Home>
2023-12-01 00:09:27 +0800

    add the solution(using binary search) for Problem 162(Find Peak Element)

--- C++/162.cpp
+++ C++/162.cpp
@@ -2,26 +2,17 @@ class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
         int n = nums.size();
-        if(n == 1)
-            return 0;
         int l = 0, r = n - 1;
-        while(l <= r){  // binary search
-            int i = (l + r) / 2;
-            if((i == 0 && nums[i + 1] < nums[i]) || (i == n - 1 && nums[i - 1] < nums[i]) || (i != 0 && i != n - 1 && nums[i - 1] < nums[i] && nums[i + 1] < nums[i]))
-                return i;
-            if(i == 0){
-                l = i + 1;
-                continue;
-            }
-            else if(i == n - 1){
-                r = i - 1;
-                continue;
-            }
-            int slope = nums[i + 1] - nums[i];
-            if(slope <= 0)  // because the slope of nums[-1] will be negative and the slope of nums[n - 1] will be positive
-                r = i - 1;
+        while(l <= r){
+            int mid = l + (r - l) / 2;
+            int left_slope = nums[mid] > (mid - 1 >= 0 ? nums[mid - 1] : LLONG_MIN) ? 1 : -1;
+            int right_slope = (mid + 1 < n ? nums[mid + 1] : LLONG_MIN) > nums[mid] ? 1 : -1;
+            if(left_slope == 1 && right_slope == -1)
+                return mid;
+            else if(left_slope == 1 && right_slope == 1)
+                l = mid + 1;
             else
-                l = i + 1;
+                r = mid - 1;
         }
         return -1;
     }
