9aac1153397ab80ace7659ce02ada463727e4613
陳漱宇 <morris@chenshuyudeMBP.Home>
2023-12-17 20:23:00 +0800

    add the solution(custom compare function for sort()) for Problem 179(Largest Number)

--- C++/179.cpp
+++ C++/179.cpp
@@ -1,23 +1,18 @@
 class Solution {
 public:
-    static bool cmp(string a, string b){
-        if(a + b > b + a)
-            return true;
-        return false;
+    static bool cmp(string& a, string& b){
+        return a + b > b + a;
     }
     string largestNumber(vector<int>& nums) {
         string ans;
-        int n = nums.size();
-        vector<string> nums_str;
-        for(int i = 0; i < n; i++){
-            nums_str.push_back(to_string(nums[i]));
+        vector<string> v;
+        for(int num: nums){
+            v.push_back(to_string(num));
         }
-        sort(nums_str.begin(), nums_str.end(), cmp);
-        if(nums_str[0] == "0")
-            return "0";
-        for(int i = 0; i < n; i++){
-            ans += nums_str[i];
+        sort(v.begin(), v.end(), cmp);
+        for(string& s: v){
+            ans += s;
         }
-        return ans;
+        return ans[0] == '0' ? "0" : ans;
     }
 };
