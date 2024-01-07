c9dd7a8dacfeb418164b842321da2a5f986c9bf2
陳漱宇 <morris@chenshuyudeMacBook-Pro.local>
2024-01-07 22:48:01 +0800

    add the solution(using bottom-up DP) for Problem 64(Minimum Path Sum)

---
+++ C++/64.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<int> dp(n, 0);
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(j == 0)
+                    dp[j] = grid[i][j] + dp[j];
+                else if(i == 0)
+                    dp[j] = grid[i][j] + dp[j - 1];
+                else
+                    dp[j] = grid[i][j] + min(dp[j], dp[j - 1]);
+            }
+        }
+        return dp[n - 1];
+    }
+};
