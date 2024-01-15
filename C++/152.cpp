098387585ff1e6c4c10241473aac30ac0b2bc5aa
陳漱宇 <morris@chenshuyudeMacBook-Pro.local>
2024-01-15 23:28:54 +0800

    add the solution(using Kadane's Algorithm) for Problem 152(Maximum Product Subarray)

--- C++/152.cpp
+++ C++/152.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    int maxProduct(vector<int>& nums) { // using Kadane's Algorithm
-        int n = nums.size();
-        int product = 1;
+    int maxProduct(vector<int>& nums) {
+        // using Kadane's Algorithm
+        // time complexity: O(N), space complexity: O(1)
+        // reference: https://leetcode.com/problems/maximum-product-subarray/solutions/183483/java-c-python-it-can-be-more-simple/
         int ans = INT_MIN;
+        int n = nums.size();
+        int product = 0;
         for(int i = 0; i < n; i++){
-            product *= nums[i];
-            if(ans < product) 
-                ans = product;
             if(product == 0)
                 product = 1;
+            product *= nums[i];
+            ans = max(ans, product);
         }
         product = 1;
-        for(int i = n - 1; i >= 0; i--){  // avoid the situation like {-8,5,3,1,6}
-            product *= nums[i];
-            if(ans < product)
-                ans = product;
+        for(int i = n - 1; i >= 0; i--){
             if(product == 0)
                 product = 1;
+            product *= nums[i];
+            ans = max(ans, product);
         }
         return ans;
     }
