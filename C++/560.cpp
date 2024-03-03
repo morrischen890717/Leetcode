15750c67f1cf09712dea24a0d768804b30d66d3b
陳漱宇 <morris@chenshuyudeMacBook-Pro.local>
2024-03-04 01:55:41 +0800

    add the solution(using prefix sum and unordered_map) for Problem 560(Subarray Sum Equals K)

--- C++/560.cpp
+++ C++/560.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+        // using prefix sum and unordered_map
+        // time complexity: O(N), space complexity: O(N)
         int n = nums.size();
-        unordered_map<int, int> prefix;
-        prefix[0] = 1;
-        int ans = 0;
-        int sum = 0;
+        int ans = 0, prefix = 0;
+        unordered_map<int, int> cnt;
+        cnt[0] = 1;
         for(int i = 0; i < n; i++){
-            sum += nums[i];
-            if(prefix.count(sum - k) > 0)
-                ans += prefix[sum - k];
-            prefix[sum]++;
+            prefix += nums[i];
+            if(cnt.count(prefix - k))
+                ans += cnt[prefix - k];
+            cnt[prefix]++;
         }
         return ans;
     }
