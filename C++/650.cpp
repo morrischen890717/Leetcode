1f2ebe21aa47a6f77e0097c3258724f196cbe696
Morris <morris890717@gmail.com>
2024-03-18 02:11:33 +0800

    add the solution(using top-down DP) for Problem 650(2 Keys Keyboard)

--- C++/650.cpp
+++ C++/650.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    int solve(int n, vector<int>& dp){
+    int solve(vector<int>& dp, int n){
         if(dp[n] != INT_MAX)
             return dp[n];
-        for(int i = 1; i <= sqrt(n); i++){
-            if(n % i == 0){
-                int j = n / i; // n = i * j (j times of i or i times of j)
-                if(i != 1) // avoid infinite loop
-                    dp[n] = min(dp[n], solve(j, dp) + i); // copy 1 time, paste i - 1 times
-                dp[n] = min(dp[n], solve(i, dp) + j); // copy 1 time, paste j - 1 time
-            }
+        for(int i = 1; i <= n; i++){
+            if(n % i == 0)
+                dp[n] = min(dp[n], solve(dp, i) + n / i); // (n/i) includes 1 copy and (n/i - 1) pastes
         }
         return dp[n];
     }
     int minSteps(int n) {
         vector<int> dp(n + 1, INT_MAX);
         dp[1] = 0;
-        return solve(n, dp);
+        return solve(dp, n);
     }
 };
