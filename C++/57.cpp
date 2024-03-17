629ab500a57c5b51ecab782959f80272b93bac7f
Morris <morris890717@gmail.com>
2024-03-18 02:13:06 +0800

    add the solution for Problem 57(Insert Interval)

--- C++/57.cpp
+++ C++/57.cpp
@@ -1,41 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        int n = intervals.size();
-        int insert_id = 0, erase_s = -1, erase_e = -2;
-        int new_s = newInterval[0], new_e = newInterval[1];
-        for(int i = 0; i < n; i++){
-            if(newInterval[0] >= intervals[i][0] && newInterval[0] <= intervals[i][1]){
-                new_s = intervals[i][0];
-                insert_id = erase_s = i;
+        intervals.push_back({INT_MAX, INT_MAX});
+        vector<vector<int>> ans;
+        for(vector<int>& interval: intervals){
+            if(newInterval[0] == INT_MAX || interval[1] < newInterval[0]) // newInterval[0] == INT_MAX means that we have already inserted the merged newInterval, so we only need to insert the rest of the intervals
+                ans.push_back(interval);
+            else if(interval[0] > newInterval[1]){
+                ans.push_back(newInterval);
+                newInterval = {INT_MAX, INT_MAX};
+                ans.push_back(interval);
             }
-            else if(i == 0 && newInterval[0] < intervals[i][0]){
-                new_s = newInterval[0];
-                insert_id = erase_s = i;
-            }
-            else if((i == n - 1 && newInterval[0] > intervals[i][1]) || (newInterval[0] > intervals[i][1] && newInterval[0] < intervals[i + 1][0])){
-                new_s = newInterval[0];
-                insert_id = erase_s = i + 1;
-            }
-            if(newInterval[1] >= intervals[i][0] && newInterval[1] <= intervals[i][1]){
-                new_e = intervals[i][1];
-                erase_e = i;
-                break;
-            }
-            else if(i == 0 && newInterval[1] < intervals[i][1]){
-                new_e = newInterval[1];
-                erase_e = -3;
-                break;
-            }
-            else if((i == n - 1 && newInterval[1] > intervals[i][1]) || (newInterval[1] > intervals[i][1] && newInterval[1] < intervals[i + 1][0])){
-                new_e = newInterval[1];
-                erase_e = i;
-                break;
-            }
+            else
+                newInterval = {min(newInterval[0], interval[0]), max(newInterval[1], interval[1])};
         }
-        if(erase_s <= erase_e)
-            intervals.erase(intervals.begin() + erase_s, intervals.begin() + erase_e + 1);
-        intervals.insert(intervals.begin() + insert_id, {new_s, new_e});
-        return intervals;
+        ans.pop_back();
+        return ans;
     }
 };
