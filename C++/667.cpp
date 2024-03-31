f8c476bc973b3a9b09b2e0e900c0a2b953722de8
Morris <morris890717@gmail.com>
2024-03-31 21:40:39 +0800

    add the solution(using Greedy) for Problem 667(Beautiful Arrangement II)

--- C++/667.cpp
+++ C++/667.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     vector<int> constructArray(int n, int k) {
-        vector<int> ans(n);
-        ans[0] = 1;
-        int id = 1;
-        int sign = 1, diff = n - 1;
+        // using Greedy
+        // time complexity: O(N)
+        vector<int> ans = {1};
+        k--;
+        int sign = -1;
+        int d = n;
         /*
         the first (k-1) |diff|, will be (n-1), (n-2), ..., (n-k), which means the first (k-1) element in answer will be [1, n, 2, n - 1, 3, n - 2, ...]
         And the rest of elements in answer will be |diff| = 1
         */
-        while(k > 1){
-            ans[id] = ans[id - 1] + sign * diff;
-            diff--;
-            sign = -sign;
-            id++;
-            k--;
-        }
-        diff = 1;
-        for(; id < n; id++){
-            ans[id] = ans[id - 1] + sign * diff;
+        for(int i = 0; i < n - 1; i++){
+            if(k >= 0){
+                sign = -sign;
+                d = k == 0 ? 1 : d - 1;
+                k--;
+            }
+            ans.push_back(ans.back() + sign * d);
         }
         return ans;
     }
