9e63cc118f7e1c32da34d890f9449b07d80b7de9
Morris <morris890717@gmail.com>
2024-04-02 02:10:11 +0800

    add the solution(using Greedy) for Problem 665(Non-decreasing Array)

--- C++/665.cpp
+++ C++/665.cpp
@@ -3,22 +3,24 @@ public:
     bool checkPossibility(vector<int>& nums) {
         int n = nums.size();
         int cnt = 0;
-        for(int i = 1; i < n - 1; i++){
-            if(nums[i] < nums[i - 1] && nums[i + 1] < nums[i])
-                return false;
-            else if(nums[i] < nums[i - 1]){
-                if(cnt >= 1)
+        for(int i = 0; i < n; i++){
+            int l1 = i - 1 >= 0 ? nums[i - 1] : INT_MIN, l2 = i - 2 >= 0 ? nums[i - 2] : INT_MIN, r = i == n - 1 ? INT_MAX : nums[i + 1];
+            if(nums[i] < l1){
+                /*
+                if l1 <= r:
+                    let nums[i] = l1
+                else if l2 <= nums[i]:
+                    let l1 = l2
+                else if l2 > nums[i]:
+                    impossible
+                */
+                if(l1 <= r)
+                    nums[i] = l1;
+                else if(l2 > nums[i])
                     return false;
-                cnt++; // modify nums[i - 1]
-            }
-            else if(nums[i + 1] < nums[i]){
-                if(cnt >= 1)
-                    return false;
-                if(nums[i + 1] < nums[i - 1])
-                    nums[i + 1] = nums[i]; // modify nums[i + 1]
-                else
-                    nums[i] = nums[i - 1]; // modify nums[i]
                 cnt++;
+                if(cnt > 1)
+                    return false;
             }
         }
         return true;
