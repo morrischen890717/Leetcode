e3a4fb02defb11a5c933529286cc891bfd4c6768
Morris <morris890717@gmail.com>
2024-04-17 18:26:52 +0800

    add the solution(using Greedy) for Problem 738(Monotone Increasing Digits)

--- C++/738.cpp
+++ C++/738.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
     int monotoneIncreasingDigits(int n) {
-        int digitCnt = 0;
-        int tmp = n;
-        for(tmp = n; tmp; tmp /= 10){ // count the number of digits
-            digitCnt++;
-        }
-        int prevDigit = -1;
-        int prevId = -1; // prevId stands for the index of digit which is strictly smaller than current digit
-        tmp = n;
-        for(int i = digitCnt - 1; i >= 0; i--){
-            int num = pow(10, i);
-            int curDigit = tmp / num;
-            if(curDigit < prevDigit){
-                n /= pow(10, prevId);
-                return n * pow(10, prevId) - 1;
+        int ans = 0;
+        int prev = 10;
+        int cnt = 0;
+        while(n){
+            int cur = n % 10;
+            if(cur > prev){
+                ans = cur * pow(10, cnt) - 1;
+                prev = cur - 1;
             }
-            else if(curDigit > prevDigit){
-                prevDigit = curDigit;
-                prevId = i;
+            else{
+                ans = cur * pow(10, cnt) + ans;
+                prev = cur;
             }
-            tmp %= num;
+            cnt++;
+            n /= 10;
         }
-        return n;
+        return ans;
     }
 };
