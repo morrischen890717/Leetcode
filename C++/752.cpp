83f569ea368d24832665568b9150dd5264775185
Morris <morris890717@gmail.com>
2024-04-22 02:50:47 +0800

    add the solution(using BFS) for Problem 752(Open the Lock)

--- C++/752.cpp
+++ C++/752.cpp
@@ -1,33 +1,40 @@
 class Solution {
 public:
     int openLock(vector<string>& deadends, string target) {
-        queue<string> q;
+        // using BFS
+        int step = 0;
+        queue<int> q;
         vector<bool> visited(10000, false);
-        int turns = 0;
         for(string& deadend: deadends){
             visited[stoi(deadend)] = true;
         }
-        q.push("0000");
+        q.push(stoi(target));
+        visited[stoi(target)] = true;
         while(!q.empty()){
-            int size = q.size();
-            for(int i = 0; i < size; i++){
-                string cur = q.front();
+            int n = q.size();
+            for(int i = 0; i < n; i++){
+                int cur = q.front();
                 q.pop();
-                if(cur == target)
-                    return turns;
-                if(visited[stoi(cur)])
-                    continue;
-                visited[stoi(cur)] = true;
+                if(cur == 0)
+                    return step;
                 for(int j = 0; j < 4; j++){
-                    string next = cur;
-                    next[j] = next[j] == '9' ? '0' : next[j] + 1;
-                    q.push(next);
-                    next = cur;
-                    next[j] = next[j] == '0' ? '9' : next[j] - 1;
-                    q.push(next);
+                    int num = pow(10, j);
+                    int increase = cur + num, decrease = cur - num;
+                    if(increase / (num * 10) != cur / (num * 10))
+                        increase -= num * 10;
+                    if(decrease < 0 || decrease / (num * 10) != cur / (num * 10))
+                        decrease += num * 10;
+                    if(!visited[increase]){
+                        visited[increase] = true;
+                        q.push(increase);
+                    }
+                    if(!visited[decrease]){
+                        visited[decrease] = true;
+                        q.push(decrease);
+                    }
                 }
             }
-            turns++;
+            step++;
         }
         return -1;
     }
