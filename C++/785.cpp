24712ec34b23c2f1bb33a96ba3516d36a90d97d5
Morris <morris890717@gmail.com>
2024-05-04 02:52:45 +0800

    add the solution(using DFS) for Problem 785(Is Graph Bipartite?)

--- C++/785.cpp
+++ C++/785.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     bool dfs(vector<vector<int>>& graph, vector<int>& visited, int i, int group){
-        if(visited[i] != -1)
+        if(visited[i] != 0)
             return visited[i] == group;
         visited[i] = group;
         for(int j = 0; j < graph[i].size(); j++){
-            if(!dfs(graph, visited, graph[i][j], group ^ 1))
+            if(!dfs(graph, visited, graph[i][j], -group))
                 return false;
         }
         return true;
     }
     bool isBipartite(vector<vector<int>>& graph) {
         int n = graph.size();
-        vector<int> visited(n, -1);
+        vector<int> visited(n, 0);
         for(int i = 0; i < n; i++){
-            if(!dfs(graph, visited, i, visited[i]))
+            if(visited[i] == 0 && !dfs(graph, visited, i, 1))
                 return false;
         }
         return true;
