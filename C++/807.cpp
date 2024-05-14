fad1ffadf4d59f44048dbf7cd48c5db17a046054
Morris <morris890717@gmail.com>
2024-05-15 01:56:07 +0800

    add the solution for Problem 807(Max Increase to Keep City Skyline)

--- C++/807.cpp
+++ C++/807.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     int maxIncreaseKeepingSkyline(vector<vector<int>>& grid) {
-        int n = grid.size();
         int ans = 0;
-        vector<int> maxHeight(2 * n, 0);
-        for(int i = 0; i < n; i++){
+        int m = grid.size(), n = grid[0].size();
+        vector<int> row(m, 0), col(n, 0);
+        for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                maxHeight[i] = max(maxHeight[i], grid[i][j]);
-                maxHeight[j + n] = max(maxHeight[j + n], grid[i][j]);
+                row[i] = max(row[i], grid[i][j]);
+                col[j] = max(col[j], grid[i][j]);
             }
         }
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                ans += (min(maxHeight[i], maxHeight[j + n]) - grid[i][j]);
+                ans += min(row[i], col[j]) - grid[i][j];
             }
         }
         return ans;
