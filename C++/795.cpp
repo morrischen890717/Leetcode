647ffb89cc137c13fd53eec5da8c703d87e7a0a3
Morris <morris890717@gmail.com>
2024-05-15 02:02:29 +0800

    add the solution(using two-pointers) for Problem 795(Number of Subarrays with Bounded Maximum)

--- C++/795.cpp
+++ C++/795.cpp
@@ -3,22 +3,18 @@ public:
     int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
         // using two-pointers
         // time complexity: O(N), space complexity: O(1)
-        int n = nums.size();
         int ans = 0;
-        int l = 0;
-        int pos = -1; // the position of the last element which value is in the valid range
-        int validCnt = 0; // # of elements which value is in the valid range
+        int l = 0, last = -1; // last stands for the last position which val is between left and right
+        int n = nums.size();
         for(int r = 0; r < n; r++){
             if(nums[r] > right){
                 l = r + 1;
-                validCnt = 0;
+                last = -1;
             }
-            else if(nums[r] >= left && nums[r] <= right){
-                validCnt++;
-                pos = r;
-            }
-            if(validCnt)
-                ans += (pos - l + 1);
+            else if(left <= nums[r] && nums[r] <= right)
+                last = r;
+            if(last != -1) // means that at least one element which val is between left and right
+                ans += (last - l + 1);
         }
         return ans;
     }
