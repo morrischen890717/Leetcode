db9600bb51b678116e81c3829dbf8eda8f032011
Morris <morris890717@gmail.com>
2024-05-15 01:53:11 +0800

    add the solution for Problem 1219(Path with Maximum Gold)

--- C++/1219.cpp
+++ C++/1219.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>>& grid, vector<int>& dirs, int i, int j){
-        int max_val = grid[i][j];
-        int tmp = grid[i][j];
+    int solve(vector<vector<int>>& grid, vector<int>& dirs, int m, int n, int i, int j){
+        if(i < 0 || i >= m || j < 0 || j >= n || grid[i][j] == 0)
+            return 0;
+        int sum = 0;
+        int amount = grid[i][j];
+        grid[i][j] = 0; // visited
         for(int k = 0; k < 4; k++){
             int new_i = i + dirs[k], new_j = j + dirs[k + 1];
-            if(new_i >= 0 && new_i < grid.size() && new_j >= 0 && new_j < grid[0].size() && grid[new_i][new_j] != 0){
-                grid[i][j] = 0; // visited
-                max_val = max(max_val, tmp + dfs(grid, dirs, new_i, new_j));
-                grid[i][j] = tmp; // have not visited yet
-            }
+            sum = max(sum, amount + solve(grid, dirs, m, n, new_i, new_j));
         }
-        return max_val;
+        grid[i][j] = amount;
+        return sum;
     }
     int getMaximumGold(vector<vector<int>>& grid) {
+        int ans = 0;
         int m = grid.size(), n = grid[0].size();
         vector<int> dirs = {-1, 0, 1, 0, -1};
-        int ans = 0;
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(grid[i][j] != 0)
-                   ans = max(ans, dfs(grid, dirs, i, j));
+                ans = max(ans, solve(grid, dirs, m, n, i, j));
             }
         }
         return ans;
