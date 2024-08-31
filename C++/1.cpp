086608f260837fd8d33fe62894f9425901368e9b
Morris <morris890717@gmail.com>
2024-09-01 01:43:49 +0800

    add the solution(using unordered_map) for Problem 1(Two Sum)

--- C++/1.cpp
+++ C++/1.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        using namespace std;
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        int num_size = nums.size();
-        vector<int> ans;
-        for(int i = 0; i < num_size; i++){
-            for(int j = i + 1; j < num_size; j++){
-                if(nums[i] + nums[j] == target){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                    return ans;
-                }
-            }
+        unordered_map<int, int> m;
+        for(int i = 0; i < nums.size(); i++){
+            if(m.count(target - nums[i]))
+                return {i, m[target - nums[i]]};
+            m[nums[i]] = i;
         }
-        return ans;
+        return {-1, -1};
     }
 };
