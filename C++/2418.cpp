25c4145c297d5d0d61d9d7d6ca53b97ea0a322f1
Morris <morris890717@gmail.com>
2024-09-01 03:20:37 +0800

    add the solution for Problem 2418(Sort the People)

---
+++ C++/2418.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+        vector<pair<int, string>> v;
+        for(int i = 0; i < names.size(); i++){
+            v.push_back({heights[i], names[i]});
+        }
+        sort(v.begin(), v.end());
+        for(int i = 0; i < names.size(); i++){
+            names[i] = v[names.size() - 1 - i].second;
+        }
+        return names;
+    }
+};
