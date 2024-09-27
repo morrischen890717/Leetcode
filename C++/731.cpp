f9ba4214fe5dd37c488c8936603b7cf101d96e04
Morris <morris890717@gmail.com>
2024-09-27 23:54:37 +0900

    add the solution for Problem 731(My Calendar II)

--- C++/731.cpp
+++ C++/731.cpp
@@ -1,27 +1,25 @@
 class MyCalendarTwo {
 public:
-    map<int, int> m;
+    vector<pair<int, int>> bookings, overlap;
     MyCalendarTwo() {
+
+    }
+
+    bool isOverlap(pair<int, int>& e, int start, int end){
+        return max(e.first, start) < min(e.second, end);
     }
     
     bool book(int start, int end) {
-        /*
-        reference: https://leetcode.com/problems/my-calendar-ii/solutions/323479/simple-c-solution-using-built-in-map-same-as-253-meeting-rooms-ii/
-        if there is no clash, a 'start' is always be followed by its corresponding end, so 'booked' will never be more than 1. 
-        If two time periods are clashing, you will see two consecutive starts. 
-        So, if there is only a double clash 'booked' can grow to 2. Similarly, if there is a triple clash then 'booked' can go to 3.
-        */
-        m[start]++;
-        m[end]--;
-        int booked = 0;
-        for(map<int, int>::iterator it = m.begin(); it != m.end(); it++){
-            booked += it->second;
-            if(booked >= 3){
-                m[start]--;
-                m[end]++;
+        // reference: https://leetcode.com/problems/my-calendar-ii/solutions/5838400/understand-once-you-will-solve-on-your-own-no-array-used-less-code-with-added-video/?envType=daily-question&envId=2024-09-27
+        for(pair<int, int>& e: overlap){
+            if(isOverlap(e, start, end))
                 return false;
-            }
         }
+        for(pair<int, int>& e: bookings){
+            if(isOverlap(e, start, end))
+                overlap.push_back({max(e.first, start), min(e.second, end)});
+        }
+        bookings.push_back({start, end});
         return true;
     }
 };
