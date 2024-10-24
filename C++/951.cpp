8208a063e8a24a1e81dcece520cda03e816e83f7
Morris <morris890717@gmail.com>
2024-10-25 02:33:47 +0900

    add the solution(using DFS) for Problem 951(Flip Equivalent Binary Trees)

--- C++/951.cpp
+++ C++/951.cpp
@@ -12,25 +12,10 @@
 class Solution {
 public:
     bool flipEquiv(TreeNode* root1, TreeNode* root2) {
-        // using DFS
-        if(root1 == NULL || root2 == NULL){
-            return root1 == root2;
-        }
-        if(root1->val != root2->val)
+        if(!root1 && !root2)
+            return true;
+        if(!root1 || !root2 || root1->val != root2->val)
             return false;
-        int root1Left = root1->left ? root1->left->val : -1;
-        int root1Right = root1->right ? root1->right->val : -1;
-        int root2Left = root2->left ? root2->left->val : -1;
-        int root2Right = root2->right ? root2->right->val : -1;
-        if(root1Left != root2Left || root1Right != root2Right){
-            if(root1Right == root2Left && root1Left == root2Right){
-                TreeNode *l = root1->left;
-                root1->left = root1->right;
-                root1->right = l;
-            }
-            else
-                return false;
-        }
-        return flipEquiv(root1->left, root2->left) && flipEquiv(root1->right, root2->right);
+        return flipEquiv(root1->left, root2->left) && flipEquiv(root1->right, root2->right) || flipEquiv(root1->left, root2->right) && flipEquiv(root1->right, root2->left);
     }
 };
