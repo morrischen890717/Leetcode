ed2bd21eeb9e57d664246f51aefa368db9b07829
Morris <morris890717@gmail.com>
2025-02-10 13:59:31 +0900

    add the solution for Problem 3174(Clear Digits)

---
+++ C++/3174.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        stack<char> st;
+        for(char c: s){
+            if(isdigit(c))
+                st.pop();
+            else
+                st.push(c);
+        }
+        string ans;
+        while(!st.empty()){
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(), ans.end());
+        return ans;
+    }
+};
