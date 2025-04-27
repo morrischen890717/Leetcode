c8f7f6a92dfe326a12875a5ed5557c436c7ca419
Morris <morris890717@gmail.com>
2025-04-28 02:16:22 +0900

    add the solution for Problem 3392(Count Subarrays of Length Three With a Condition)

---
+++ C++/3392.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countSubarrays(vector<int>& nums) {
+        int ans = 0;
+        int n = nums.size();
+        for(int i = 0; i < n - 2; i++){
+            if((nums[i] + nums[i + 2]) * 2 == nums[i + 1])
+                ans++;
+        }
+        return ans;
+    }
+};
